06e13f91c4808e2d7f1964a53c8c8f99743994e4
Mohammad Reza Mansouri <beekalam@gmail.com>
2016-07-28 09:49:17 +0430

    LogsController@showLogs

--- app/Http/Controllers/LogsController.php
+++ app/Http/Controllers/LogsController.php
@@ -5,20 +5,28 @@ namespace App\Http\Controllers;
 use App\Log;
 use App\ConnectionLog;
 use App\ConnectionLogDetail;
+
 class LogsController
 {
-	public function show($page)	
+	private $per_page = 10;
+	public function show($page)
 	{
-		$per_page = 10;
+		$per_page = $this->per_page;
 		$offset = ($page * $per_page) - $per_page;
 		$ret = Log::orderBy('log_id')->skip($offset)->take($per_page)->get();
 		// return json_encode(["data"=> $ret]);
 		return $ret;
 	}
 
-	public function showt($user_id, $page)
+	public function showLogs($user_id, $page)
 	{
-		// return ConnectionLog::all();
-		return ConnectionLogDetail::all();
+		$per_page = $this->per_page;
+		$offset = ($page * $per_page) - $per_page;
+
+		$data = ConnectionLog::where('user_id', '=', $user_id)->skip($offset)->take($per_page)->get();
+		$ret = ["data" => $data];
+		return json_encode($ret);
 	}
+
+
 }
